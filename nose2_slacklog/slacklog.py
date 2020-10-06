78c240dccb4a7fe8a500bdf5f2d4030ec5d17194
Takashi Masuyama <mamewotoko@gmail.com>
2020-10-06 22:09:07 +0900

    make log format simple

--- nose2_slacklog/slacklog.py
+++ nose2_slacklog/slacklog.py
@@ -13,7 +13,16 @@ class SlackLog(Plugin):
             'webhook_url': self.config.as_str('webhook_url', default=None),
             'username':  self.config.as_str('username', default='bot')
         }
-        slack_handler = SlackLogHandler(self._config['webhook_url'], username=self._config['username'])
+        slack_handler = SlackLogHandler(self._config['webhook_url'],
+                                        username=self._config['username'],
+                                        emojis={
+                                            logging.INFO: ":white_circle:",
+                                            logging.DEBUG: ":black_circle:",
+                                            logging.ERROR: ":red_circle:"
+                                        })
+        logFormat = "%(asctime)s %(levelname)s %(message)s"
+        formatter = logging.Formatter(fmt=logFormat)
+        slack_handler.setFormatter(formatter)
         self.logger = logging.getLogger("slacklog")
         self.logger.setLevel(logging.DEBUG)
         self.logger.addHandler(slack_handler)
